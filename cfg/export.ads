c155811fe29bec8e04f2fdfbad00edee941908cf
Anil Poriya <anil@altiscale.com>
2019-12-11 14:31:15 -0800

    feat(BDSBLUE-480): exporting hadoop-lzo jar and tar of native libraries to nexus

--- cfg/export.ads
+++ cfg/export.ads
@@ -1,18 +1,12 @@
-Properties properties = new Properties()
-File propertiesFile = new File("$gendir/src/hadoop-lzo-artifacts/build.properties")
-propertiesFile.withInputStream {
-    properties.load(it)
-}
-
 artifacts builderVersion: "1.1", {
 
   group "com.sap.bds.ats-altiscale", {
 
     artifact "hadoop-lzo", {
-      file "$gendir/src/hadoop-lzo-artifacts/hadoop-lzo-${properties.ARTIFACT_VERSION}.jar"
+      file "$gendir/src/hadoop-lzo-artifacts/hadoop-lzo-${buildVersion}.jar", extension: "jar"
     }
     artifact "hadoop-lzo-libgplcompression", {
-      file "$gendir/src/hadoop-lzo-artifacts/hadoop-lzo-libgplcompression-${properties.ARTIFACT_VERSION}.tar.gz"
+      file "$gendir/src/hadoop-lzo-artifacts/hadoop-lzo-libgplcompression-${buildVersion}.tar.gz"
     }
   }
 }
--- sap-xmake-build.sh
+++ sap-xmake-build.sh
@@ -11,7 +11,6 @@ export M2_HOME=/opt/mvn3
 export JAVA_HOME=/opt/sapjvm_7
 export PATH=$M2_HOME/bin:$JAVA_HOME/bin:$PATH
 DATE_STRING=`date +%Y%m%d%H%M%S`
-ARTIFACT_VERSION=0.4.18-${DATE_STRING}
 
 #------------------------------------------------------------------------------
 #
@@ -33,17 +32,16 @@ if [[ "$?" -ne 0 ]] ; then
 fi
 
 #---------------------------------------------------------------------------------------------
+#
 #  ***** exporting hadoop-lzo jar and native libraries required for hadoop packaging *****
+#
 #---------------------------------------------------------------------------------------------
 
 ARTIFACT_DIR="$MY_DIR/hadoop-lzo-artifacts"
 mkdir --mode=0755 -p ${ARTIFACT_DIR}
 
-# exporting the ARTIFACT_VERSION in properties file to use when exporting the hadoop-lzo artifacts to nexus
-echo "ARTIFACT_VERSION=${ARTIFACT_VERSION}" > ${ARTIFACT_DIR}/build.properties
+mv $MY_DIR/target/hadoop-lzo-[0-9]*.[0-9]*.[0-9]*-[0-9]*[0-9].jar $ARTIFACT_DIR/hadoop-lzo-${XMAKE_PROJECT_VERSION}.jar
+cd $MY_DIR/target/native/Linux-amd64-64/
+tar -czvf "$ARTIFACT_DIR/hadoop-lzo-libgplcompression-${XMAKE_PROJECT_VERSION}.tar.gz" lib/
 
-#mv $MY_DIR/target/hadoop-lzo-[0-9]*.[0-9]*.[0-9]*-[0-9]*[0-9].jar $ARTIFACT_DIR/hadoop-lzo-${ARTIFACT_VERSION}.jar
-#cd $MY_DIR/target/native/Linux-amd64-64/
-#tar -czvf "$ARTIFACT_DIR/hadoop-lzo-libgplcompression-${ARTIFACT_VERSION}.tar.gz" lib/
-
-#exit 0
+exit 0
